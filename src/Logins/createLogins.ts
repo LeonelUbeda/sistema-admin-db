cbf76837599ff0f6d8e5e73a2fb7cc408dc2f3bf
LABB-PC19 <LABC204@LABC204-PC19.uam.net.ni>
2019-12-11 16:20:52 -0600

    Roles y logins

---
+++ src/Logins/createLogins.ts
@@ -0,0 +1,158 @@
+import database from '../Database/database'
+
+const createLogins = async () => {
+    // anular
+    await database.query(`
+    USE [taller]
+GO
+
+
+DECLARE @RoleName sysname
+set @RoleName = N'mecanico'
+
+IF @RoleName <> N'public' and (select is_fixed_role from sys.database_principals where name = @RoleName) = 0
+BEGIN
+    DECLARE @RoleMemberName sysname
+    DECLARE Member_Cursor CURSOR FOR
+    select [name]
+    from sys.database_principals 
+    where principal_id in ( 
+        select member_principal_id
+        from sys.database_role_members
+        where role_principal_id in (
+            select principal_id
+            FROM sys.database_principals where [name] = @RoleName AND type = 'R'))
+
+    OPEN Member_Cursor;
+
+    FETCH NEXT FROM Member_Cursor
+    into @RoleMemberName
+    
+    DECLARE @SQL NVARCHAR(4000)
+
+    WHILE @@FETCH_STATUS = 0
+    BEGIN
+        
+        SET @SQL = 'ALTER ROLE '+ QUOTENAME(@RoleName,'[') +' DROP MEMBER '+ QUOTENAME(@RoleMemberName,'[')
+        EXEC(@SQL)
+        
+        FETCH NEXT FROM Member_Cursor
+        into @RoleMemberName
+    END;
+
+    CLOSE Member_Cursor;
+    DEALLOCATE Member_Cursor;
+END
+/****** Object:  DatabaseRole [mecanico]    Script Date: 11/12/2019 16:14:11 ******/
+DROP ROLE [mecanico]
+GO
+
+/****** Object:  DatabaseRole [mecanico]    Script Date: 11/12/2019 16:14:11 ******/
+CREATE ROLE [mecanico]
+GO
+
+
+USE [taller]
+GO
+
+
+DECLARE @RoleName sysname
+set @RoleName = N'consultor'
+
+IF @RoleName <> N'public' and (select is_fixed_role from sys.database_principals where name = @RoleName) = 0
+BEGIN
+    DECLARE @RoleMemberName sysname
+    DECLARE Member_Cursor CURSOR FOR
+    select [name]
+    from sys.database_principals 
+    where principal_id in ( 
+        select member_principal_id
+        from sys.database_role_members
+        where role_principal_id in (
+            select principal_id
+            FROM sys.database_principals where [name] = @RoleName AND type = 'R'))
+
+    OPEN Member_Cursor;
+
+    FETCH NEXT FROM Member_Cursor
+    into @RoleMemberName
+    
+    DECLARE @SQL NVARCHAR(4000)
+
+    WHILE @@FETCH_STATUS = 0
+    BEGIN
+        
+        SET @SQL = 'ALTER ROLE '+ QUOTENAME(@RoleName,'[') +' DROP MEMBER '+ QUOTENAME(@RoleMemberName,'[')
+        EXEC(@SQL)
+        
+        FETCH NEXT FROM Member_Cursor
+        into @RoleMemberName
+    END;
+
+    CLOSE Member_Cursor;
+    DEALLOCATE Member_Cursor;
+END
+/****** Object:  DatabaseRole [consultor]    Script Date: 11/12/2019 16:14:08 ******/
+DROP ROLE [consultor]
+GO
+
+/****** Object:  DatabaseRole [consultor]    Script Date: 11/12/2019 16:14:08 ******/
+CREATE ROLE [consultor]
+GO
+
+
+USE [taller]
+GO
+
+
+DECLARE @RoleName sysname
+set @RoleName = N'adminSistema'
+
+IF @RoleName <> N'public' and (select is_fixed_role from sys.database_principals where name = @RoleName) = 0
+BEGIN
+    DECLARE @RoleMemberName sysname
+    DECLARE Member_Cursor CURSOR FOR
+    select [name]
+    from sys.database_principals 
+    where principal_id in ( 
+        select member_principal_id
+        from sys.database_role_members
+        where role_principal_id in (
+            select principal_id
+            FROM sys.database_principals where [name] = @RoleName AND type = 'R'))
+
+    OPEN Member_Cursor;
+
+    FETCH NEXT FROM Member_Cursor
+    into @RoleMemberName
+    
+    DECLARE @SQL NVARCHAR(4000)
+
+    WHILE @@FETCH_STATUS = 0
+    BEGIN
+        
+        SET @SQL = 'ALTER ROLE '+ QUOTENAME(@RoleName,'[') +' DROP MEMBER '+ QUOTENAME(@RoleMemberName,'[')
+        EXEC(@SQL)
+        
+        FETCH NEXT FROM Member_Cursor
+        into @RoleMemberName
+    END;
+
+    CLOSE Member_Cursor;
+    DEALLOCATE Member_Cursor;
+END
+/****** Object:  DatabaseRole [adminSistema]    Script Date: 11/12/2019 16:14:04 ******/
+DROP ROLE [adminSistema]
+GO
+
+/****** Object:  DatabaseRole [adminSistema]    Script Date: 11/12/2019 16:14:04 ******/
+CREATE ROLE [adminSistema]
+GO
+
+
+
+    `)
+
+}
+
+export default createLogins
