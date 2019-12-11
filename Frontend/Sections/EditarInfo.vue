b5d483591fb1ee4664d66ef098af52a19a9ca84f
Luz Clarita <luzpm@uamv.edu.ni>
2019-12-11 01:33:21 -0600

    cambiar contrasena

--- Frontend/Sections/EditarInfo.vue
+++ Frontend/Sections/EditarInfo.vue
@@ -7,14 +7,18 @@
           <h2 class="text-2xl">Editar usuario {{$store.state.Usuario.usuario}}</h2>
         </div>
       </div>
-      <div class="w-full max-w-xs md:items-center">
-        <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" id="contenedor">
+      <div class="w-full max-w-xs flex content-start">
+        <div
+          class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex content-start"
+          id="contenedor"
+        >
           <div class="mb-4">
             <label class="block text-gray-700 text-sm font-bold mb-4" for="nueva">Nueva Contraseña</label>
             <input
               type="password"
               class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
               placeholder="Contraseña"
+              v-model="contrasena"
             />
             <button
               class="flex items-center justify-between button mb-4"
@@ -48,6 +52,7 @@ export default {
       axios
         .put(url, { contrasena: this.contrasena })
         .then(respuesta => {
+          msjActualizar();
           console.log(respuesta.data);
         })
         .catch(err => console.log(err));
@@ -78,7 +83,12 @@ export default {
   margin-right: auto;
   border-radius: 8px;
 }
-// .contenedor {
-//   justify-content: center;
-// }
+.contenedor {
+  display: flex !important;
+  justify-content: center !important;
+  align-items: center !important;
+
+  resize: both;
+  overflow: auto;
+}
 </style>
